65de9d2c104969912b5061c6a8bb84b6a4b0ef08
taddes <taddes.korris@gmail.com>
2018-05-20 23:24:45 -0400

    added all HTTP requests in api.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const express = require('express');
 // Set up Express
 const app = express();
 
-app.get('/api', function(req, res){
-  console.log('GET request')
-  res.send({name:'Taddes'});
-});
 
 // Listen for requests
 app.listen(process.env.port || 4000, function() {
---
+++ routes/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// Get a list of cats from database
+router.get('/cats', function(req, res) {
+  res.send({type:'GET'})
+});
+
+// Add new cat
+router.post('/cats', function(req, res) {
+  res.send({type: 'POST'})
+});
+
+// update a cat from database
+router.put('/cats/:id', function(req, res) {
+  res.send({type: 'PUT'})
+});
+
+// delete a cat from db
+router.delete('/cats/:id', function(req, res) {
+  res.send({type: 'DELETE'})
+});
