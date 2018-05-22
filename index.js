6625d47fd4054d4b643c94b1fe388cba6f413412
taddes <taddes.korris@gmail.com>
2018-05-21 23:02:18 -0400

    added initial .catch error handler

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(bodyParser.json());
 // Middleware. Adds the /api url to routes. 
 app.use('/api', routes);
 
+// Error handling middleware
+App.use(function(err, req, res, next){
+  console.log(err);
+});
 // Listen for requests
 app.listen(process.env.port || 4000, function() {
 console.log(`Listening for requests on port 4000`)
